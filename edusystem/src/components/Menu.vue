70fc002f2fa06d3b44fad8c2c50d40f25ab250cf
TYZhugo <2084188242@qq.com>
2022-03-02 21:42:47 +0800

    改了菜单栏的样式和图标

--- edusystem/src/components/Menu.vue
+++ edusystem/src/components/Menu.vue
@@ -2,37 +2,40 @@
 <div class='qqq'>
   <div class="ppp">
   <img :src="imgUrl1"/>
-  
   </div>
   <h2>123</h2>
+  <hr/>
   <el-menu
     default-active="2"
+    background-color="#909399"
     class="el-menu-vertical-demo"
     :collapse="isCollapse"
     @open="handleOpen"
     @close="handleClose"
     router='ture'
+    text-color="white"
+    active-text-color="#303133"
   >
     <el-menu-item index="/">
       <template #title ><div class="tupian"><img :src="imgUrl2"></div>我的班级</template>
     </el-menu-item>
-    <el-menu-item index="/Build">
-      <template #title ><div class="tupian"><img :src="imgUrl2"></div>创建试卷</template>
-    </el-menu-item>
     <el-menu-item index="/Grades">
-      <template #title ><div class="tupian"><img :src="imgUrl2"></div>成绩管理</template>
-    </el-menu-item>
-    <el-menu-item index="/">
-      <template #title ><div class="tupian"><img :src="imgUrl2"></div>考试发布</template>
-    </el-menu-item>
-    <el-menu-item index="/Manage">
-      <template #title><div class="tupian"><img :src="imgUrl2"></div>实时监控</template>
-    </el-menu-item>
-    <el-menu-item index="5">
-      <template #title><div class="tupian"><img :src="imgUrl2"></div>判卷</template>
+      <template #title ><div class="tupian"><img :src="imgUrl3"></div>成绩管理</template>
     </el-menu-item>
     <el-menu-item index="/Question">
-      <template #title><div class="tupian"><img :src="imgUrl2"></div>我的题库</template>
+      <template #title ><div class="tupian"><img :src="imgUrl2"></div>我的题库</template>
+    </el-menu-item>
+    <el-menu-item index="/">
+      <template #title ><div class="tupian"><img :src="imgUrl2"></div>实时监控</template>
+    </el-menu-item>
+    <el-menu-item index="/Manage">
+      <template #title><div class="tupian"><img :src="imgUrl6"></div>考试发布</template>
+    </el-menu-item>
+    <el-menu-item index="Build">
+      <template #title><div class="tupian"><img :src="imgUrl7"></div>创建试卷</template>
+    </el-menu-item>
+    <el-menu-item index="/">
+      <template #title><div class="tupian"><img :src="imgUrl2"></div>批改卷子</template>
     </el-menu-item>
   </el-menu>
 </div>
@@ -44,7 +47,11 @@ export default {
   data(){
     return{
       imgUrl1:require("../assets/touxiang.png"),
-      imgUrl2:require("../assets/banji.png")
+      imgUrl2:require("../assets/banji.png"),
+      imgUrl3:require("../assets/guanli.png"),
+      imgUrl6:require("../assets/fabu.png"),
+      imgUrl7:require("../assets/chuangjian.png")
+      
     }
   }
 }
@@ -53,11 +60,15 @@ export default {
 <style>
 .qqq{
   width:300px;
+  height: 760px;
   text-align: center;
+  background-color: #909399;
+
 }
 .ppp{
   display: flex;
-  height:200px;
+  width: 300px;
+  height:160px;
   justify-content: center;
   align-items: center;
 }
@@ -66,7 +77,6 @@ export default {
 }
 .tupian{
   display: flex;
-  background-color: rgb(5, 172, 228);
   height:45px;
   width: 45px;
   justify-content: center;
@@ -76,5 +86,13 @@ export default {
 .tupian img{
   width:40px;
   height: 40px;
+}
+.MenuDivier{
+  width: 250px;
+}
+.qqq hr{
+  align-content: center;
+  width: 250px;
+  color: white;
 }
 </style>
