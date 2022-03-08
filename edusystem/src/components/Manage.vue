4d1c88b1f8b98f095f5a915314a5855b676084e5
TYZhugo <2084188242@qq.com>
2022-03-08 21:41:11 +0800

    完成了题库界面

--- edusystem/src/components/Manage.vue
+++ edusystem/src/components/Manage.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
   <Header></Header>
+  <div class="main">
   <Menu></Menu>
+  <div class="manageCard">
+    <div v-for="i in 6" v-bind:key="i" class="box1"><img :src="imgUrl1"><div class="examText"><p>大学物理</p><p>2021.1.30 8: 00--10:00</p></div></div>
+  </div>
+  </div>
 </div>
 </template>
 
@@ -13,10 +18,18 @@ export default {
     components:{
         Header,
         Menu
+    },
+    data(){
+      return{
+        imgUrl1:require("../assets/exampicture.png")
+      }
     }
 }
 </script>
 
 <style>
-
+.manageCard{
+  display: flex;
+  flex-wrap: wrap;
+}
 </style>
--- edusystem/src/components/Question.vue
+++ edusystem/src/components/Question.vue
@@ -1,8 +1,26 @@
 <template>
 <div>
   <Header></Header>
+  <div class="main">
   <Menu></Menu>
-  
+  <div class="questionMain">
+  <div class="questionCard">
+    <div class="questionName">
+    <img :src="imgUrl1">
+    <p>大学物理</p>
+    </div>
+    <br/>
+    <p class="questionNumber">共计1010道题</p>
+  </div>
+  <div class="questionBuild">
+  <p>添加新题：</p>
+  <input placeholder="选择添加新题型">
+  </div>
+  <textarea placeholder="请输入题目"></textarea>
+  <br/>
+  <button class="questionButton">添加</button>
+  </div>
+  </div>
 </div>
 </template>
 
@@ -14,10 +32,75 @@ export default {
     components:{
         Header,
         Menu
+    },
+    data(){
+      return{
+        imgUrl1:require("../assets/exampicture.png")
+      }
+    },
+    methods:{
+
     }
 }
 </script>
 
 <style>
-
+.questionCard{
+  background-color: white;
+  width: 300px;
+  height: 200px;
+  border-radius: 15px;
+  margin-left: 80px;
+  margin-top: 20px;
+}
+.questionCard img{
+  width: 30px;
+  height: 30px;
+  margin-top: 50px;
+  margin-left: 60px;
+}
+.questionName p{
+  font-size: 20px;
+  margin-left: 50px;
+  margin-top: 50px;
+}
+.questionName{
+    display: flex;
+}
+.questionNumber{
+  margin-left: 100px;
+  margin-top: 50px;
+}
+.questionBuild{
+  display: flex;
+  margin-top: 100px;
+  margin-left: 50px;
+}
+.questionBuild input{
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  width: 300px;
+  height: 27px;
+  margin-left: 5px;
+}
+.questionMain textarea{
+  border: none;
+  outline: none;
+  border-radius: 10px;
+  width: 450px;
+  height: 200px;
+  margin-left: 50px;
+  margin-top: 20px;
+}
+.questionButton{
+  width: 200px;
+  height: 50px;
+  border: none;
+  border-radius: 15px;
+  outline: none;
+  background-color: aliceblue;
+  margin-left: 350px;
+  margin-top: 50px;
+}
 </style>
