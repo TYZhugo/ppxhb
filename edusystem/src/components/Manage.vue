bb257ab411664a0c62a2d22f429a9909a72cd7f8
TYZhugo <2084188242@qq.com>
2022-03-19 19:48:03 +0800

    完善了试题批改的功能

--- edusystem/src/components/Manage.vue
+++ edusystem/src/components/Manage.vue
@@ -2,7 +2,7 @@
   <div>
   <div class="main">
   <div class="manageCard">
-    <div v-for="i in 6" v-bind:key="i" class="box1"><img :src="imgUrl1"><div class="examText"><p>大学物理</p><p>2021.1.30 8: 00--10:00</p></div></div>
+    <div v-for="i in 6" v-bind:key="i" class="box1" @click="correct"><img :src="imgUrl1"><div class="examText"><p>大学物理</p><p>2021.1.30 8: 00--10:00</p></div></div>
   </div>
   </div>
 </div>
@@ -17,6 +17,11 @@ export default {
       return{
         imgUrl1:require("../assets/exampicture.png")
       }
+    },
+    methods:{
+      correct(){
+        this.$router.push('/Correct')
+      }
     }
 }
 </script>
@@ -25,5 +30,8 @@ export default {
 .manageCard{
   display: flex;
   flex-wrap: wrap;
+}
+.box1{
+  cursor: pointer;
 }
 </style>
