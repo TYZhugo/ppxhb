cd8f3fc2cb93a140fdac031bde6a63994dd1ebe7
TYZhugo <2084188242@qq.com>
2022-03-19 19:58:52 +0800

    新建题库页面

---
+++ edusystem/src/components/Questionshow.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="questionshowcard">
+      <p>选择题</p>
+      <p>{{content}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Queationshow',
+    data(){
+        return{
+            style:this.$route.query.style,
+            content:this.$route.query.content
+        }
+    }
+}
+</script>
+
+<style>
+.questionshowcard{
+    margin-left: 60px;
+    margin-top: 50px;
+    border-radius: 10px;
+    width: 700px;
+    height: 700px;
+    text-align: center;
+    background-color: white;
+}
+</style>
