631e0e433cdf556fae5c09b74350323125438557
TYZhugo <2084188242@qq.com>
2022-03-19 19:51:08 +0800

    进一步完善监控提醒功能

--- edusystem/src/components/Jiankong.vue
+++ edusystem/src/components/Jiankong.vue
@@ -5,19 +5,36 @@
             <option>大一一班</option>
             <option>大一二班</option>
         </select>
-        <div class="wranging"><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" data-v-ba633cb8=""><path fill="currentColor" d="M512 64a448 448 0 1 1 0 896 448 448 0 0 1 0-896zm0 192a58.432 58.432 0 0 0-58.24 63.744l23.36 256.384a35.072 35.072 0 0 0 69.76 0l23.296-256.384A58.432 58.432 0 0 0 512 256zm0 512a51.2 51.2 0 1 0 0-102.4 51.2 51.2 0 0 0 0 102.4z"></path></svg>警告</div>
+        <div class="wranging" @click="jgchange"><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" data-v-ba633cb8=""><path fill="currentColor" d="M512 64a448 448 0 1 1 0 896 448 448 0 0 1 0-896zm0 192a58.432 58.432 0 0 0-58.24 63.744l23.36 256.384a35.072 35.072 0 0 0 69.76 0l23.296-256.384A58.432 58.432 0 0 0 512 256zm0 512a51.2 51.2 0 1 0 0-102.4 51.2 51.2 0 0 0 0 102.4z"></path></svg>提醒</div>
       </div>
         <el-scrollbar   height="700px">
             <div class="JKface">
-        <div v-for="i in 30" v-bind:key="i" class="JKcard"><input type="radio"/></div>
+        <div v-for="i in 30" v-bind:key="i" class="JKcard"><input type="radio" v-if="jgshow"/></div>
             </div>
         </el-scrollbar>
   </div>
 </template>
 
 <script>
+import { ElMessage } from 'element-plus'
 export default {
-    name:'Jiankong'
+    name:'Jiankong',
+    data(){
+        return{
+            jgshow:false
+        }
+    },
+    methods:{
+        jgchange(){
+            this.jgshow=!this.jgshow
+            if(!this.jgshow){
+            ElMessage({
+                message: '已提醒该同学.',
+                type: 'warning',
+            })
+            }
+        }
+    }
 }
 </script>
 
